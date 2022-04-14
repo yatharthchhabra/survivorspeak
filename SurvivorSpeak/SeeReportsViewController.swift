ce3c69bb4a556f4e5ea22123d0ac1fb95af2d486
Yatharth Chhabra <yatharthchhabra@gmail.com>
2022-04-14 11:32:19 -0400

    search works

--- SurvivorSpeak/SeeReportsViewController.swift
+++ SurvivorSpeak/SeeReportsViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 import Firebase
 
-class SeeReportsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+class SeeReportsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var searchLabel: UILabel!
     let db = Firestore.firestore()
 
     var reports:[String] = []
@@ -18,9 +21,17 @@ class SeeReportsViewController: UIViewController, UITableViewDelegate, UITableVi
         super.viewDidLoad()
         reportsTable.delegate = self
         reportsTable.dataSource = self
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
+        if let searchTextField = self.searchBar.value(forKey: "searchField") as? UITextField , let clearButton = searchTextField.value(forKey: "_clearButton")as? UIButton {
+
+            clearButton.addTarget(self, action: #selector(self.clearSearch), for: .touchUpInside)
+        }
+        searchLabel.text = "Search Results for: "
     }
+    
     override func viewDidAppear(_ animated: Bool) {
+        reports = []
         db.collection("Reports").getDocuments() { (querySnapshot, err) in
             if let err = err {
                 print("Error getting documents: \(err)")
@@ -58,6 +69,74 @@ class SeeReportsViewController: UIViewController, UITableViewDelegate, UITableVi
           return cell
       }
 
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchLabel.text = "Search Results for: \(searchBar.text!)"
+        db.collection("Reports").getDocuments() { [self] (querySnapshot, err) in
+            reports = []
+
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    var data = document.data()
+                    
+                    var name = data["name"] as! String
+                    var phoneNum = data["phone"] as! String
+                    var emailAddr = data["email"] as! String
+                    var dateText = data["date"] as! String
+                    var countyText = data["county"] as! String
+                    var positionText = data["position"] as! String
+                    var offenderText = data["offender"] as! String
+                    var detailsText = data["details"] as! String
+                    
+                    let firstSection = "On " + dateText + ", in " + countyText + " county, "
+                    let secondSection = positionText + " " + offenderText + " commited an infraction as described below : "
+                    var finalText =  firstSection + secondSection + detailsText
+                    
+                    print(offenderText + "--" + searchBar.text!)
+                    if(offenderText.contains(searchBar.text!) || searchBar.text!.contains(offenderText)){
+                        self.reports.append(finalText)
+                        print(self.reports)
+                    }
+                }
+            }
+            DispatchQueue.main.async { self.reportsTable.reloadData() }
+
+        }
+    }
+    @objc func clearSearch() {
+        print("cancel")
+        searchLabel.text = "Search Results for: "
+
+        db.collection("Reports").getDocuments() { [self] (querySnapshot, err) in
+            reports = []
+
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    var data = document.data()
+                    
+                    var name = data["name"] as! String
+                    var phoneNum = data["phone"] as! String
+                    var emailAddr = data["email"] as! String
+                    var dateText = data["date"] as! String
+                    var countyText = data["county"] as! String
+                    var positionText = data["position"] as! String
+                    var offenderText = data["offender"] as! String
+                    var detailsText = data["details"] as! String
+                    
+                    let firstSection = "On " + dateText + ", in " + countyText + " county, "
+                    let secondSection = positionText + " " + offenderText + " commited an infraction as described below : "
+                    var finalText =  firstSection + secondSection + detailsText
+                    self.reports.append(finalText)
+
+                }
+            }
+            DispatchQueue.main.async { self.reportsTable.reloadData() }
+
+        }
+    }
     /*
     // MARK: - Navigation
 
